3cc1349adcc7c8c6060b0b6ea1bff019ef68b279
11 <mobileappdev@bths.edu>
2026-05-22 14:38:23 -0400

    Initial Commit

---
+++ Kirby2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kirby2.0
+//
+//  Created by Student on 5/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kirby2.0/Kirby2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Kirby2_0App.swift
+//  Kirby2.0
+//
+//  Created by Student on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Kirby2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
