b461c7391356953f92637dfcad6b60f31aa05e05
michellel314 <michelle.lee.087@gmail.com>
2026-05-27 20:26:55 -0400

    I finished learning how to add effects to the background moving with Kirby

--- Kirby2.0/GameScene.swift
+++ Kirby2.0/GameScene.swift
@@ -14,7 +14,7 @@ class GameScene: SKScene {
     override func didMove(to view: SKView){
         setupScene()
         setupPlayer()
-        setupBackground(imageName: "Dreamscape")
+        setupBackground(imageName: "Dreamscape", duration: 10, zPos: 1, scale: 1)
         
     }
     
@@ -31,7 +31,7 @@ class GameScene: SKScene {
     private func setupPlayer(){
         let player = SKSpriteNode(imageNamed: "tile000")
         player.position = CGPoint(x: 50 + player.size.width, y: player.size.height + 20)
-        player.setScale(1.5)
+        player.setScale(1)
         player.zPosition = 4
         // set up animation
         let textureAtlas = SKTextureAtlas(named: "Kirby")
@@ -47,18 +47,48 @@ class GameScene: SKScene {
         addChild(player)
     }
     
-    private func setupBackground(imageName : String){
-        let layer = SKSpriteNode(imageNamed: imageName)
+    private func setupBackground(imageName : String, duration: Double, zPos: CGFloat, scale: CGFloat){
+        let numCopies = determinNumberOfCopies(imageName: imageName, scale: scale)
+        for i in 0..<numCopies{
+            let layer = SKSpriteNode(imageNamed: imageName)
+            
+            // calculate scaled size
+            let heightRatio = size.height / layer.size.height
+            let layerHeight = size.height * scale
+            let layerWidth = layer.size.width * heightRatio * scale
+            let layerSize = CGSize(width: layerWidth, height: layerHeight)
+            layer.size = layerSize
+            
+            layer.position = CGPoint(x: layerSize.width * CGFloat(i), y: 0)
+            layer.anchorPoint = .zero
+            layer.zPosition = zPos
+            
+            //animate layer so it moves off the screen
+            let move = SKAction.moveBy(x: -layer.size.width, y: 0, duration: 10)
+            let wrap = SKAction.moveBy(x: layer.size.width, y: 0, duration: 0)
+            let sequence = SKAction.sequence([move, wrap])
+            let moveForever = SKAction.repeatForever(sequence)
+            layer.run(moveForever)
+            addChild(layer)
+        }
         
-        // calculate scaled size
+    }
+    
+    private func determinNumberOfCopies(imageName: String, scale: CGFloat) -> Int {
+        let layer = SKSpriteNode(imageNamed: imageName)
+    
+        //calculate scaled size
         let heightRatio = size.height / layer.size.height
-        let layerHeight = size.height
-        let layerWidth = layer.size.width * heightRatio
+        let layerHeight = size.height * scale
+        let layerWidth = layer.size.width * heightRatio * scale
         let layerSize = CGSize(width: layerWidth, height: layerHeight)
-        layer.size = layerSize
-        layer.position = CGPoint(x: 0, y: 0)
-        layer.anchorPoint = .zero
-        layer.zPosition = 1
-        addChild(layer)
+        
+        //divide the width of the screen by width of layer, round up to the nearest integer (ceiling) and convert to int
+        let howManyPartiallyFit = Int(size.width / layerSize.width)
+        
+        // we need 1 more than how many partially fit
+        let howManyNeeded = howManyPartiallyFit + 1
+        return howManyNeeded
+        
     }
 }
